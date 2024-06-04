332a9f1b693f83c9abc798ab10f6f93a8a9b91d7
K0bus <contact@k0bus.fr>
2024-06-04 02:23:09 +0200

    Pretty test

--- test.js
+++ test.js
@@ -10,20 +10,29 @@ WarcraftLog.connect(
     if(connected)
     {
         WarcraftLog.getCharacterByName("kobus", "uldaman", "eu").then(json => {
-            console.log(json)
+            if(json !== null) console.log("- ✅   getCharacterByName tested")
+            else console.log("- ❌   getCharacterByName tested")
         });
         WarcraftLog.getCharacterById(23648878).then(json => {
-            console.log(json)
+            if(json !== null) console.log("- ✅   getCharacterById tested")
+            else console.log("- ❌   getCharacterById tested")
         });
 
         WarcraftLog.getGuildByName("nocturnys", "uldaman", "eu").then(json => {
-            console.log(json)
+            if(json !== null) console.log("- ✅   getGuildByName tested")
+            else console.log("- ❌   getGuildByName tested")
         });
         WarcraftLog.getGuildById(60437).then(json => {
-            console.log(json)
+            if(json !== null) console.log("- ✅   getGuildById tested")
+            else console.log("- ❌   getGuildById tested")
         });
         WarcraftLog.getReportByCode("wfmb4XGT1PFajn2g").then(json => {
-            console.log(json)
+            if(json !== null) console.log("- ✅   getReportByCode tested")
+            else console.log("- ❌   getReportByCode tested")
+        });
+        WarcraftLog.getReportsByGuild("nocturnys", "uldaman", "eu").then(json => {
+            if(json !== null) console.log("- ✅   getReportsByGuild tested")
+            else console.log("- ❌   getReportsByGuild tested")
         });
     }
 })
